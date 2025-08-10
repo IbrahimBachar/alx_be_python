2386cee5b9677d0d2367aa10a705518d2014cdc5
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-08-10 23:56:21 +0200

    Simple bank account creation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,10 +7,11 @@ class BankAccount:
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
+            self.account_balance -= amount
             return True
         else:
             return False
     
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}.0")
     
